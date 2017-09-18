4405e843a0815a48d0dfac5f35acfb74ac114832
Tom Adams <tomjadams@gmail.com>
2017-09-19 07:38:18 +1000

    Handle both timeouts & errors from the cache in the same way.

--- src/main/scala/com/redbubble/util/fetch/FetchRunner.scala
+++ src/main/scala/com/redbubble/util/fetch/FetchRunner.scala
@@ -4,21 +4,17 @@ import cats.syntax.either._
 import com.redbubble.util.cache.{CacheKey, SimpleCache}
 import com.redbubble.util.fetch.TwitterFutureFetchMonadError.twitterFutureFetchMonadError
 import com.twitter.conversions.time._
-import com.twitter.util.{Await, Future, FuturePool, TimeoutException}
+import com.twitter.util.{Await, Future, FuturePool}
 import fetch._
 
 final case class FetchedObjectCache(underlyingCache: SimpleCache) extends DataSourceCache {
-  /**
-    * We rescue failures & timeouts from the underlying cache by returning None, which will cause a fetch from the
-    * datasource.
-    */
   override def get[A](k: DataSourceIdentity): Option[A] = {
-    val cacheGet = underlyingCache.get[A](CacheKey(k.toString)).rescue {
-      case _ => Future.value(None)
+    // Failures & timeouts from the underlying cache return None, causing a fetch from the datasource.
+    val result = Either.catchNonFatal {
+      val cacheGetFuture = underlyingCache.get[A](CacheKey(k.toString))
+      Await.result(cacheGetFuture, timeout = 5.seconds)
     }
-    Either.catchOnly[TimeoutException] {
-      Await.result(cacheGet, timeout = 5.seconds)
-    }.getOrElse(None)
+    result.getOrElse(None)
   }
 
   override def update[A](k: DataSourceIdentity, v: A): FetchedObjectCache = {
